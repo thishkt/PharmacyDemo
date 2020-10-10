9becd7745480573a57bfc9b21879b3b74c692101
侯光燦 <tw-hkt@hotmail.com.tw>
2020-10-10 20:11:18 +0800

    1. 口罩資料網址，改定義在 Constants 2. 獲取口罩資料，改使用封裝過的 OkHttp

---
+++ app/src/main/java/com/thishkt/pharmacydemo/Constants.kt
@@ -0,0 +1,3 @@
+package com.thishkt.pharmacydemo
+
+const val PHARMACIES_DATA_URL="https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
--- app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
+import com.thishkt.pharmacydemo.Util.OkHttpUtil
+import com.thishkt.pharmacydemo.Util.OkHttpUtil.Companion.mOkHttpUtil
 import com.thishkt.pharmacydemo.data.PharmacyInfo
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
@@ -24,36 +26,12 @@ class MainActivity : AppCompatActivity() {
         //顯示忙碌圈圈
         progressBar.visibility = View.VISIBLE
 
-        //口罩資料網址
-        val pharmaciesDataUrl =
-            "https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
-
-        //Part 1: 宣告 OkHttpClient
-        val okHttpClient = OkHttpClient().newBuilder().build()
-
-        //Part 2: 宣告 Request，要求要連到指定網址
-        val request: Request = Request.Builder().url(pharmaciesDataUrl).get().build()
-
-        //Part 3: 宣告 Call
-        val call = okHttpClient.newCall(request)
-
-        //執行 Call 連線後，採用 enqueue 非同步方式，獲取到回應的結果資料
-        call.enqueue(object : Callback {
-            override fun onFailure(call: Call, e: java.io.IOException) {
-                Log.d("HKT", "onFailure: $e")
-
-                //關閉忙碌圈圈
-                progressBar.visibility = View.GONE
-            }
-
-            override fun onResponse(call: Call, response: Response) {
+        mOkHttpUtil.getAsync(PHARMACIES_DATA_URL, object : OkHttpUtil.ICallback {
+            override fun onResponse(response: Response) {
                 //藥局名稱變數宣告
                 var propertiesName = StringBuilder()
 
                 val pharmaciesData = response.body?.string()
-
-                //將 pharmaciesData 整包字串資料，轉成 JSONObject 格式
-                val obj = JSONObject(pharmaciesData)
 
                 val pharmacyInfo = Gson().fromJson(pharmaciesData, PharmacyInfo::class.java)
 
@@ -61,15 +39,19 @@ class MainActivity : AppCompatActivity() {
                     propertiesName.append(i.property.name + "\n")
                 }
 
-
                 runOnUiThread {
-                    //最後取得所有藥局名稱資料，指定顯示到 TextView 元件中
                     tv_pharmacies_data.text = propertiesName
 
                     //關閉忙碌圈圈
                     progressBar.visibility = View.GONE
                 }
+            }
 
+            override fun onFailure(e: okio.IOException) {
+                Log.d("HKT", "onFailure: $e")
+
+                //關閉忙碌圈圈
+                progressBar.visibility = View.GONE
             }
         })
     }
