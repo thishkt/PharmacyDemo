e4d3e1a6cf04be742d48fc345bf9c25a82907106
侯光燦 <tw-hkt@hotmail.com.tw>
2020-10-10 17:55:44 +0800

    使用TextView_ScrollView_將口罩資料顯示在畫面上

--- app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
@@ -2,13 +2,9 @@ package com.thishkt.pharmacydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.telecom.Call
 import android.util.Log
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import okio.IOException
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,14 +31,18 @@ class MainActivity : AppCompatActivity() {
 
         //執行 Call 連線後，採用 enqueue 非同步方式，獲取到回應的結果資料
         call.enqueue(object : Callback {
-            override fun onFailure(call: okhttp3.Call, e: java.io.IOException) {
+            override fun onFailure(call: Call, e: java.io.IOException) {
                 Log.d("HKT", "onFailure: $e")
             }
 
-            override fun onResponse(call: okhttp3.Call, response: Response) {
-                Log.d("HKT", "onResponse: ${response.body?.string()}")
+            override fun onResponse(call: Call, response: Response) {
+                val pharmaciesData = response.body?.string()
+
+                runOnUiThread{
+                    //將 Okhttp 獲取到的回應值，指定到畫面的 TextView 元件中
+                    tv_pharmacies_data.text = pharmaciesData
+                }
             }
         })
-
     }
 }
