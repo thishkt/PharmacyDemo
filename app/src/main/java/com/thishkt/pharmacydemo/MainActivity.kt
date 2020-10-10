ae8abe657d1365d0370e20677dd844a789b5f0d9
侯光燦 <tw-hkt@hotmail.com.tw>
2020-10-10 17:42:50 +0800

    OkHttp 基本拉資料方式 (GET 請求範例)

--- app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
@@ -2,10 +2,47 @@ package com.thishkt.pharmacydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Call
+import android.util.Log
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okio.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getPharmacyData()
+
+    }
+
+    private fun getPharmacyData() {
+        //口罩資料網址
+        val pharmaciesDataUrl =
+                "https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
+
+        //Part 1: 宣告 OkHttpClient
+        val okHttpClient = OkHttpClient().newBuilder().build()
+
+        //Part 2: 宣告 Request，要求要連到指定網址
+        val request: Request = Request.Builder().url(pharmaciesDataUrl).get().build()
+
+        //Part 3: 宣告 Call
+        val call = okHttpClient.newCall(request)
+
+        //執行 Call 連線後，採用 enqueue 非同步方式，獲取到回應的結果資料
+        call.enqueue(object : Callback {
+            override fun onFailure(call: okhttp3.Call, e: java.io.IOException) {
+                Log.d("HKT", "onFailure: $e")
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: Response) {
+                Log.d("HKT", "onResponse: ${response.body?.string()}")
+            }
+        })
+
     }
 }
