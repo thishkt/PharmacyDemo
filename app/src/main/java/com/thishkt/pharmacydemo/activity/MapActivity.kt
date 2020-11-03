e40f25e012391a854feab968475ce211a0c7bae0
HKT <thishkt@gmail.com>
2020-11-03 22:24:36 +0800

    獲取位置權限名稱修改

--- app/src/main/java/com/thishkt/pharmacydemo/activity/MapActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/activity/MapActivity.kt
@@ -13,14 +13,17 @@ import com.thishkt.pharmacydemo.REQUEST_LOCATION_PERMISSION
 
 class MapActivity : AppCompatActivity() {
 
+    private var locationPermissionGranted = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        getCurrentLocation()
+        getLocationPermission()
     }
 
-    private fun getCurrentLocation() {
+    private fun getLocationPermission() {
         //檢查權限
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -28,7 +31,10 @@ class MapActivity : AppCompatActivity() {
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             //已獲取到權限
-            //todo 獲取經緯度
+            //todo 獲取經緯度 getDeviceLocation
+
+            locationPermissionGranted = true
+
             Toast.makeText(this, "已獲取到位置權限，可以準備開始獲取經緯度", Toast.LENGTH_SHORT).show()
         } else {
             //詢問要求獲取權限
@@ -42,14 +48,16 @@ class MapActivity : AppCompatActivity() {
             )
         ) {
             AlertDialog.Builder(this)
-                .setMessage("需要位置權限")
+                .setMessage("此功能需要位置權限")
                 .setPositiveButton("確定") { _, _ ->
                     ActivityCompat.requestPermissions(
                         this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                         REQUEST_LOCATION_PERMISSION
                     )
                 }
-                .setNegativeButton("取消") { _, _ -> requestLocationPermission() }
+                .setNegativeButton("取消") { _, _ ->
+                    Toast.makeText(this, "未獲得位置權限，功能將會無法正常使用", Toast.LENGTH_SHORT).show()
+                }
                 .show()
         } else {
             ActivityCompat.requestPermissions(
@@ -67,8 +75,9 @@ class MapActivity : AppCompatActivity() {
             REQUEST_LOCATION_PERMISSION -> {
                 if (grantResults.isNotEmpty()) {
                     if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        //權限已成功獲取
-                        getCurrentLocation()
+                        //已獲取到權限
+                        //todo 獲取經緯度 getDeviceLocation
+                        locationPermissionGranted = true
                     } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
                         if (!ActivityCompat.shouldShowRequestPermissionRationale(
                                 this,
@@ -76,13 +85,13 @@ class MapActivity : AppCompatActivity() {
                             )
                         ) {
                             //權限被永久拒絕
-                            Toast.makeText(this, "位置權限被永久拒絕", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "位置權限已被永久拒絕，功能將會無法正常使用", Toast.LENGTH_SHORT).show()
 
                             //todo 前往設定頁
                             //todo 顯示對話視窗
                         } else {
                             //權限被拒絕
-                            Toast.makeText(this, "位置權限被拒絕", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "位置權限被拒絕，功能將會無法正常使用", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
