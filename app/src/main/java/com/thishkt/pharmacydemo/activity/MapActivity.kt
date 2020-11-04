897656db011d2750664ea0b7a28eb253d84d3402
HKT <thishkt@gmail.com>
2020-11-04 22:37:31 +0800

    加入死要獲得位置權限，沒獲得不能用的邏輯

--- app/src/main/java/com/thishkt/pharmacydemo/activity/MapActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/activity/MapActivity.kt
@@ -1,30 +1,35 @@
 package com.thishkt.pharmacydemo.activity
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.thishkt.pharmacydemo.R
 import com.thishkt.pharmacydemo.REQUEST_LOCATION_PERMISSION
 
 class MapActivity : AppCompatActivity() {
 
+    private var locationPermissionGranted = false
+
+    private lateinit var mLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        getCurrentLocation()
+        mLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getLocationPermission()
     }
 
-    private fun getCurrentLocation() {
+    private fun getLocationPermission() {
         //檢查權限
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -32,33 +37,44 @@ class MapActivity : AppCompatActivity() {
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             //已獲取到權限
-            //todo 獲取經緯度
             Toast.makeText(this, "已獲取到位置權限，可以準備開始獲取經緯度", Toast.LENGTH_SHORT).show()
-
-            val mLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-
-            val locationRequest = LocationRequest()
-            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            //更新頻率
-            locationRequest.interval = 1000
-            //更新次數，若沒設定，會持續更新
-            //locationRequest.numUpdates = 1
-            mLocationProviderClient.requestLocationUpdates(
-                locationRequest,
-                object : LocationCallback() {
-                    override fun onLocationResult(locationResult: LocationResult?) {
-                        locationResult ?: return
-                        Log.d(
-                            "HKT",
-                            "緯度:${locationResult.lastLocation.latitude} , 經度:${locationResult.lastLocation.longitude} "
-                        )
-                    }
-                },
-                null
-            )
+            locationPermissionGranted = true
+            getDeviceLocation()
         } else {
             //詢問要求獲取權限
             requestLocationPermission()
+        }
+    }
+
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted
+            ) {
+                val locationRequest = LocationRequest()
+                locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                //更新頻率
+                locationRequest.interval = 1000
+                //更新次數，若沒設定，會持續更新
+                //locationRequest.numUpdates = 1
+                mLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    object : LocationCallback() {
+                        override fun onLocationResult(locationResult: LocationResult?) {
+                            locationResult ?: return
+                            Log.d(
+                                "HKT",
+                                "緯度:${locationResult.lastLocation.latitude} , 經度:${locationResult.lastLocation.longitude} "
+                            )
+                        }
+                    },
+                    null
+                )
+
+            } else {
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
         }
     }
 
@@ -68,7 +84,7 @@ class MapActivity : AppCompatActivity() {
             )
         ) {
             AlertDialog.Builder(this)
-                .setMessage("需要位置權限")
+                .setMessage("此應用程式，需要位置權限才能正常使用")
                 .setPositiveButton("確定") { _, _ ->
                     ActivityCompat.requestPermissions(
                         this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -93,8 +109,9 @@ class MapActivity : AppCompatActivity() {
             REQUEST_LOCATION_PERMISSION -> {
                 if (grantResults.isNotEmpty()) {
                     if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        //權限已成功獲取
-                        getCurrentLocation()
+                        //已獲取到權限
+                        locationPermissionGranted = true
+                        getDeviceLocation()
                     } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
                         if (!ActivityCompat.shouldShowRequestPermissionRationale(
                                 this,
@@ -102,17 +119,35 @@ class MapActivity : AppCompatActivity() {
                             )
                         ) {
                             //權限被永久拒絕
-                            Toast.makeText(this, "位置權限被永久拒絕", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "位置權限已被關閉，功能將會無法正常使用", Toast.LENGTH_SHORT).show()
 
-                            //todo 前往設定頁
-                            //todo 顯示對話視窗
+                            AlertDialog.Builder(this)
+                                .setTitle("開啟位置權限")
+                                .setMessage("此應用程式，位置權限已被關閉，需開啟才能正常使用")
+                                .setPositiveButton("確定") { _, _ ->
+                                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                                    startActivityForResult(intent, REQUEST_LOCATION_PERMISSION)
+                                }
+                                .setNegativeButton("取消") { _, _ -> requestLocationPermission() }
+                                .show()
                         } else {
                             //權限被拒絕
-                            Toast.makeText(this, "位置權限被拒絕", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "位置權限被拒絕，功能將會無法正常使用", Toast.LENGTH_SHORT).show()
+                            requestLocationPermission()
                         }
                     }
                 }
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_LOCATION_PERMISSION -> {
+                getLocationPermission()
+            }
+
         }
     }
 }
