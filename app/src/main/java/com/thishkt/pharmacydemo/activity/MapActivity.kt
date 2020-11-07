c0449ce09092c773414b433984200e8454611d5e
HKT <thishkt@gmail.com>
2020-11-07 15:25:52 +0800

    調整先檢查  GPS 狀態，再獲取經緯度

--- app/src/main/java/com/thishkt/pharmacydemo/activity/MapActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/activity/MapActivity.kt
@@ -2,13 +2,16 @@ package com.thishkt.pharmacydemo.activity
 
 import android.Manifest
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -19,34 +22,33 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.thishkt.pharmacydemo.R
+import com.thishkt.pharmacydemo.REQUEST_ENABLE_GPS
 import com.thishkt.pharmacydemo.REQUEST_LOCATION_PERMISSION
 import com.thishkt.pharmacydemo.adapter.MyInfoWindowAdapter
 
-
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var locationPermissionGranted = false
+    private var mCurrLocationMarker: Marker? = null
 
-    private var googleMap: GoogleMap? = null
-    private var currLocationMarker: Marker? = null
-
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var mContext: Context
-    private lateinit var currentLocation: LatLng
-
+    private var googleMap: GoogleMap? = null
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    //台北101
     private val defaultLocation = LatLng(25.0338483, 121.5645283)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        mContext = this
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        mContext = this
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    private fun getCurrentLocation() {
+    private fun getLocationPermission() {
         //檢查權限
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -54,60 +56,102 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             //已獲取到權限
-            //todo 獲取經緯度
-            Toast.makeText(this, "已獲取到位置權限，可以準備開始獲取經緯度", Toast.LENGTH_SHORT).show()
-
-            val mLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-
-            val locationRequest = LocationRequest()
-            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            //更新頻率
-            locationRequest.interval = 1000
-            //更新次數，若沒設定，會持續更新
-            //locationRequest.numUpdates = 1
-            mLocationProviderClient.requestLocationUpdates(
-                locationRequest,
-                object : LocationCallback() {
-                    override fun onLocationResult(locationResult: LocationResult?) {
-                        locationResult ?: return
-                        Log.d(
-                            "HKT",
-                            "緯度:${locationResult.lastLocation.latitude} , 經度:${locationResult.lastLocation.longitude} "
-                        )
-
-                        currentLocation =
-                            LatLng(
-                                locationResult.lastLocation.latitude,
-                                locationResult.lastLocation.longitude
-                            )
-
-
-                        //googleMap?.clear()
-                        currLocationMarker?.remove()
-                        googleMap?.setInfoWindowAdapter(MyInfoWindowAdapter(mContext))
-                        currLocationMarker = googleMap?.addMarker(
-                            MarkerOptions()
-                                .position(currentLocation)
-                                .title("現在位置")
-                                .snippet("100,66")
-                        )
-                        currLocationMarker?.showInfoWindow()
-
-                        googleMap?.moveCamera(
-                            CameraUpdateFactory.newLatLngZoom(
-                                currentLocation, 15f
-                            )
-                        )
-                    }
-                },
-                null
-            )
+            locationPermissionGranted = true
+            checkGPSState()
         } else {
             //詢問要求獲取權限
             requestLocationPermission()
         }
     }
 
+    private fun checkGPSState() {
+        val locationManager = mContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder(mContext)
+                .setTitle("GPS 尚未開啟")
+                .setMessage("使用此功能需要開啟 GSP 定位功能")
+                .setPositiveButton("前往開啟",
+                    DialogInterface.OnClickListener { _, _ ->
+                        startActivityForResult(
+                            Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), REQUEST_ENABLE_GPS
+                        )
+                    })
+                .setNegativeButton("取消", null)
+                .show()
+        } else {
+            Toast.makeText(this, "已獲取到位置權限且GPS已開啟，可以準備開始獲取經緯度", Toast.LENGTH_SHORT).show()
+            getDeviceLocation()
+        }
+    }
+
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted
+            ) {
+                val locationRequest = LocationRequest()
+                locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                //更新頻率
+                locationRequest.interval = 1000
+                //更新次數，若沒設定，會持續更新
+                //locationRequest.numUpdates = 1
+                mFusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    object : LocationCallback() {
+                        override fun onLocationResult(locationResult: LocationResult?) {
+                            locationResult ?: return
+                            Log.d(
+                                "HKT",
+                                "緯度:${locationResult.lastLocation.latitude} , 經度:${locationResult.lastLocation.longitude} "
+                            )
+
+
+                            val currentLocation =
+                                LatLng(
+                                    locationResult.lastLocation.latitude,
+                                    locationResult.lastLocation.longitude
+                                )
+
+                            //清除所有標記
+                            //googleMap?.clear()
+
+                            //清除上一次位置標記
+                            //mCurrLocationMarker?.remove()
+
+                            //當下位置存到一個 Marker 變數中，好讓下一次可以清除
+//                            mCurrLocationMarker =googleMap?.addMarker(
+//                                MarkerOptions().position(currentLocation).title("現在位置")
+//                            )
+
+//                            googleMap?.isMyLocationEnabled = true
+
+
+                            mCurrLocationMarker?.remove()
+                            googleMap?.setInfoWindowAdapter(MyInfoWindowAdapter(mContext))
+                            mCurrLocationMarker = googleMap?.addMarker(
+                                MarkerOptions()
+                                    .position(currentLocation)
+                                    .title("現在位置")
+                                    .snippet("100,66")
+                            )
+                            mCurrLocationMarker?.showInfoWindow()
+
+                            googleMap?.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    currentLocation, 15f
+                                )
+                            )
+                        }
+                    },
+                    null
+                )
+
+            } else {
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
 
     private fun requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
@@ -140,8 +184,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             REQUEST_LOCATION_PERMISSION -> {
                 if (grantResults.isNotEmpty()) {
                     if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        //權限已成功獲取
-                        getCurrentLocation()
+                        //已獲取到權限
+                        locationPermissionGranted = true
+                        checkGPSState()
                     } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
                         if (!ActivityCompat.shouldShowRequestPermissionRationale(
                                 this,
@@ -149,13 +194,21 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                             )
                         ) {
                             //權限被永久拒絕
-                            Toast.makeText(this, "位置權限被永久拒絕", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "位置權限已被關閉，功能將會無法正常使用", Toast.LENGTH_SHORT).show()
 
-                            //todo 前往設定頁
-                            //todo 顯示對話視窗
+                            AlertDialog.Builder(this)
+                                .setTitle("開啟位置權限")
+                                .setMessage("此應用程式，位置權限已被關閉，需開啟才能正常使用")
+                                .setPositiveButton("確定") { _, _ ->
+                                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                                    startActivityForResult(intent, REQUEST_LOCATION_PERMISSION)
+                                }
+                                .setNegativeButton("取消") { _, _ -> requestLocationPermission() }
+                                .show()
                         } else {
                             //權限被拒絕
-                            Toast.makeText(this, "位置權限被拒絕", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "位置權限被拒絕，功能將會無法正常使用", Toast.LENGTH_SHORT).show()
+                            requestLocationPermission()
                         }
                     }
                 }
@@ -163,14 +216,25 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_LOCATION_PERMISSION -> {
+                getLocationPermission()
+            }
+            REQUEST_ENABLE_GPS -> {
+                checkGPSState()
+            }
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         this.googleMap = googleMap
-        getCurrentLocation()
-
         googleMap.moveCamera(
             CameraUpdateFactory.newLatLngZoom(
-                defaultLocation, 6f
+                defaultLocation, 15f
             )
         )
+        getLocationPermission()
     }
 }
