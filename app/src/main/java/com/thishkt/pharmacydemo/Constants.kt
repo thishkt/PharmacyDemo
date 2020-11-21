979112f6628f7296827a7ffee863194b27836e91
HKT <thishkt@gmail.com>
2020-11-21 12:23:50 +0800

    建立初始專案

--- app/src/main/java/com/thishkt/pharmacydemo/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.thishkt.pharmacydemo
-
-const val PHARMACIES_DATA_URL="https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
-
-const val REQUEST_LOCATION_PERMISSION = 1
