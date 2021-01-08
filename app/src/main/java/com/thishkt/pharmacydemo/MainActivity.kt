b8b05b1fe524f52e55ab5582c77b1daf6b57e6d0
HKT <thishkt@gmail.com>
2021-01-08 09:16:55 +0800

    1.升級 Android Studio 4.1.1 、 SDK 30 建置 、Koltin 1.4.21 2.移除 KAE (kotlin-android-extensions) & synthetic，更改為 View Binding 寫法。

--- app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
@@ -8,15 +8,21 @@ import com.google.gson.Gson
 import com.thishkt.pharmacydemo.Util.OkHttpUtil
 import com.thishkt.pharmacydemo.Util.OkHttpUtil.Companion.mOkHttpUtil
 import com.thishkt.pharmacydemo.data.PharmacyInfo
-import kotlinx.android.synthetic.main.activity_main.*
+import com.thishkt.pharmacydemo.databinding.ActivityMainBinding
 import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         getPharmacyData()
 
@@ -24,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun getPharmacyData() {
         //顯示忙碌圈圈
-        progressBar.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.VISIBLE
 
         mOkHttpUtil.getAsync(PHARMACIES_DATA_URL, object : OkHttpUtil.ICallback {
             override fun onResponse(response: Response) {
@@ -40,18 +46,20 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 runOnUiThread {
-                    tv_pharmacies_data.text = propertiesName
+                    binding.tvPharmaciesData.text = propertiesName
 
                     //關閉忙碌圈圈
-                    progressBar.visibility = View.GONE
+                    binding.progressBar.visibility = View.GONE
                 }
             }
 
             override fun onFailure(e: okio.IOException) {
                 Log.d("HKT", "onFailure: $e")
 
-                //關閉忙碌圈圈
-                progressBar.visibility = View.GONE
+                runOnUiThread {
+                    //關閉忙碌圈圈
+                    binding.progressBar.visibility = View.GONE
+                }
             }
         })
     }
