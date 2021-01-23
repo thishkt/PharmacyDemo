2f6178f8f2238e1070f76fb7da12fa45465163d6
HKT <thishkt@gmail.com>
2021-01-23 11:28:18 +0800

    1.升級 Android Studio 4.1.1 、 SDK 30 建置 、Koltin 1.4.21 2.移除 KAE (kotlin-android-extensions) & synthetic，更改為 View Binding 寫法。

--- app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/thishkt/pharmacydemo/MainActivity.kt
@@ -4,27 +4,27 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.thishkt.pharmacydemo.Util.OkHttpUtil
 import com.thishkt.pharmacydemo.Util.OkHttpUtil.Companion.mOkHttpUtil
 import com.thishkt.pharmacydemo.data.PharmacyInfo
-import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.*
-import org.json.JSONArray
-import org.json.JSONObject
+import com.thishkt.pharmacydemo.databinding.ActivityMainBinding
+import okhttp3.Response
 
 class MainActivity : AppCompatActivity() {
 
     //定義全域變數
     private lateinit var viewAdapter: MainAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initView()
 
@@ -40,23 +40,16 @@ class MainActivity : AppCompatActivity() {
         viewAdapter = MainAdapter()
 
         // 定義從佈局當中，拿到 recycler_view 元件，
-        recycler_view.apply {
+        binding.recyclerView.apply {
             // 透過 kotlin 的 apply 語法糖，設定 LayoutManager 和 Adapter
             layoutManager = viewManager
             adapter = viewAdapter
-
-            addItemDecoration(
-                DividerItemDecoration(
-                    this@MainActivity,
-                    DividerItemDecoration.VERTICAL
-                )
-            )
         }
     }
 
     private fun getPharmacyData() {
         //顯示忙碌圈圈
-        progressBar.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.VISIBLE
 
         mOkHttpUtil.getAsync(PHARMACIES_DATA_URL, object : OkHttpUtil.ICallback {
             override fun onResponse(response: Response) {
@@ -69,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                     viewAdapter.pharmacyList = pharmacyInfo.features
 
                     //關閉忙碌圈圈
-                    progressBar.visibility = View.GONE
+                    binding.progressBar.visibility = View.GONE
                 }
             }
 
@@ -77,7 +70,7 @@ class MainActivity : AppCompatActivity() {
                 Log.d("HKT", "onFailure: $e")
 
                 //關閉忙碌圈圈
-                progressBar.visibility = View.GONE
+                binding.progressBar.visibility = View.GONE
             }
         })
     }
